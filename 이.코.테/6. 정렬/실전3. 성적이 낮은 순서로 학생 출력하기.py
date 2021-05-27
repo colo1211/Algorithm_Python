810947ef7e3e746f1b3a9fb1ef8d3f24654313bd
colo1211 <colo1211@naver.com>
2021-05-28 00:40:26 +0900

    실전3

--- 이.코.테/6. 정렬/실전3. 성적이 낮은 순서로 학생 출력하기.py
+++ 이.코.테/6. 정렬/실전3. 성적이 낮은 순서로 학생 출력하기.py
@@ -4,7 +4,7 @@ people_info = []
 for i in range(people_num):
     temp = input().split()
     people_info.append((temp[0], int(temp[1])))
-
+# 서로 다른 자료형의 데이터는 리스트로 들어갈 때 튜플로 들어가는 것이 관행
 people_info = sorted(people_info, key=lambda s: s[1])
 
 for i in range(people_num):
