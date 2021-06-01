322cc85f049b78d7b80c25ca21faf8bc898d34ff
colo1211 <colo1211@naver.com>
2021-06-01 19:25:47 +0900

    Time exceed - 다시 해결하기

---
+++ 유형별문제집/탐색, 시뮬레이션/5. 수들의 합.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+arr= list(map(int,input().split()))
+count = 0
+# 시간 초과 ver
+def get_sum(arr):
+    sum = 0
+    for i in arr:
+        sum+=i
+    return sum
+
+for i in range(len(arr)):
+    if arr[i] == m:
+        count += 1
+    for j in range(i+1, len(arr)):
+        if get_sum(arr[i:j+1]) == m:
+            count += 1
+
+print(count)
