80d7683a33f144ad33b1f9cc339af4561adb4107
colo1211 <colo1211@naver.com>
2021-06-10 21:03:03 +0900

    .

--- 유형별문제집/이분 탐색, 그리디/9. 증가수열 만들기.py
+++ 유형별문제집/이분 탐색, 그리디/9. 증가수열 만들기.py
@@ -1,23 +1,27 @@
 n = int(input())
 arr = list(map(int, input().split()))
-
 temp = [] #뭐가 더 큰지 판별해주는 임시 저장 리스트
-
 start = 0
 end = len(arr)-1
 last_num = 0
-
+result = ''
 while start <= end:
-    temp.append((arr[start], 'L')) #왼쪽 값을 넣어준다.
-    temp.append((arr[end], 'R')) #오른쪽 값을 넣어준다.
-    if temp[0][0] < temp[1][0] and last_num < temp[0][0]:
+    if arr[start] > last_num:
+        temp.append((arr[start],'L'))
+    if arr[end] > last_num:
+        temp.append((arr[end],'R'))
+    temp.sort() # 2차원 배열을 정렬
+
+    if len(temp) == 0:# temp 배열이 길이가 0이면 종료
+        break
+    else:
+        result += temp[0][1]
         last_num = temp[0][0]
-        start +=1
-        print(temp[0][1])
-    if temp[0][0] > temp[1][0] and last_num < temp[1][0]:
-        last_num = temp[1][0]
-        end -= 1
-        print(temp[1][1])
+        if temp[0][1] == 'L':
+            start +=1
+        else :
+            end -= 1
+    temp.clear()
 
-
-print(temp)
+print(len(result))
+print(result)
